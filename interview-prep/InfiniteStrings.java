0703b077b978b7d1537e0a145ec730bed2ea80c0
William Stephens <william.a.stephens17@gmail.com>
2020-04-24 15:39:18 -0500

    InfiniteStrings solved

---
+++ interview-prep/InfiniteStrings.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the repeatedString function below.
+    static long repeatedString(String s, long n) {
+        long asInString = 0;
+        long finalAs = 0;
+        char[] chars = s.toCharArray();
+        for(char c : chars) {
+            if (c == 'a') {
+                asInString++;
+            }
+        }
+        long fullStrings = n / s.length();
+        finalAs = asInString * fullStrings;
+        long remainderChars = n % s.length();
+        for(int i=0;i<remainderChars;i++) {
+            if(chars[i] == 'a') {
+                finalAs++;
+            }
+        }
+        return finalAs;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        long n = scanner.nextLong();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long result = repeatedString(s, n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
